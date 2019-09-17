33acc7ba3bc2bafca48e89a7812319f00e618044
Andrea <andreaeva.lengyel@gmail.com>
2019-09-18 02:40:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello \nAndrea");
+     int A = 7;
+     int B = 226;
+        System.out.println(A + B);
+      float C = A/B;
+        System.out.println(C);
+
+        System.out.println(- 5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + (- 3 * 5)/8);
+        System.out.println(5 + 15/3 * 2 - 8 % 3);
+    }
+}
