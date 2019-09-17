df03178296212e129c899311486b47f5140f7eb4
Andrea <andreaeva.lengyel@gmail.com>
2019-09-18 02:59:20 +0300

    Final version of assignment

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
      int A = 7;
      int B = 226;
         System.out.println(A + B);
-      float C = A/B;
+      float C = (float)A/B;
         System.out.println(C);
 
         System.out.println(- 5 + 8 * 6);
-        System.out.println((55 + 9) % 9);
-        System.out.println(20 + (- 3 * 5)/8);
+        int x = (55 + 9) % 9;
+        System.out.println(x);
+        float y = 20 + (float)(- 3 * 5)/8;
+        System.out.println(y);
         System.out.println(5 + 15/3 * 2 - 8 % 3);
     }
 }
